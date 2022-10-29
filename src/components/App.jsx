1f152c774426312039d97ac96949d343c0b809d4
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-10-29 21:27:31 +0300

    delete comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,12 @@
-//import { useState, useEffect } from 'react';
-
 import { nanoid } from 'nanoid';
 import { ContactsForm } from './ContactsForm/ContactsForm';
 import { ContactsList } from './ContactsList/ContactsList';
 import { Section } from './Section/Section';
 import { Filter } from './Filter/Filter';
-//--------------------redux
+
 import { useSelector } from 'react-redux';
 import { selectContacts } from 'redux/contacts/contactsSelectors';
-//import { selectFilter } from 'redux/filter/filterSelectors';
+
 import { useDispatch } from 'react-redux';
 
 import { addContact, deleteContact } from 'redux/contacts/contactsSlice';
@@ -16,18 +14,7 @@ import { onFilter } from 'redux/filter/filterSlice';
 
 export const App = () => {
   const contacts = useSelector(selectContacts);
-  //const filter = useSelector(selectFilter);
   const dispath = useDispatch();
-  //   const [contacts, setContacts] = useState(
-  //     () =>
-  //       JSON.parse(window.localStorage.getItem('contacts')) ??
-  //
-  // );
-  // const [filter, setFilter] = useState('');
-
-  // useEffect(() => {
-  //   window.localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
 
   const onAddContact = data => {
     if (contacts.find(contact => contact.name === `${data.name}`)) {
@@ -38,25 +25,16 @@ export const App = () => {
       id: nanoid(),
       ...data,
     };
-    // const newContact = { id: nanoid(), name, number };
     dispath(addContact(contact));
-
-    //setContacts([contact, ...contacts]);
   };
 
   const handleChange = e => {
     const { value } = e.target;
-
-    //setFilter(value); // тут писать диспатч
     dispath(onFilter(value));
   };
 
-  // const onDeleteContact = contactId => {
-  //   setContacts(contacts.filter(contact => contact.id !== contactId));
-  // };
   const onDeleteContact = contactId => {
     dispath(deleteContact(contactId));
-    //contacts.filter(contact => contact.id !== contactId);
   };
 
   return (
@@ -67,13 +45,7 @@ export const App = () => {
 
       <Section title="Contacts">
         {<Filter handleChange={handleChange} />}
-        {contacts && (
-          <ContactsList
-            // contacts={contacts}
-            // filter={filter}
-            onDeleteContact={onDeleteContact}
-          />
-        )}
+        {contacts && <ContactsList onDeleteContact={onDeleteContact} />}
       </Section>
     </>
   );
